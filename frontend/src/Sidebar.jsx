ce3a86a99575157acdef8f746dcd0e55539c9951
Ankita Solanki <ankita@gmail.com>
2026-05-22 09:58:07 +0530

    Building Sidebar

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -2,9 +2,22 @@ import "./Sidebar.css";
 
 function Sidebar(){
     return(
-        <div>
-            Sidebar
-        </div>
+        <section className="sidebar">
+         {/* new chat button */}
+           <button>
+            <img src="src/assets/blacklogo.png"  className="logo" lt="gpt logo"></img>
+            <span><i class="fa-solid fa-pen-to-square"></i></span>
+           </button>
+         {/* chat history */}
+         <ul className="history">
+            <li>thread1</li>
+             <li>thread2</li>
+         </ul>
+         {/* sign */}
+         <div className="sign">
+            <p>By Ankita &hearts;</p>
+         </div>
+        </section>
     )
 }
 export default Sidebar;
