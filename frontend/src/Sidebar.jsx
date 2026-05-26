33c2c0baca3b75fdc5e2018a2dfb458bbf937cae
Ankita Solanki <ankita@gmail.com>
2026-05-26 16:48:59 +0530

    Delete a Thread

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -45,6 +45,24 @@ function Sidebar(){
     }
    }
 
+const deleteThread = async(threadId) => {
+   try{
+    const response= await  fetch(`http://localhost:8080/api/thread/${threadId}`,{method: "DELETE"});
+    const res = await response.json();
+    console.log(res);
+   
+   // updated threads re-render
+   setAllThreads(prev => prev.filter(thread => thread.threadId !== threadId));
+
+   if(threadId === currThreadId){
+      createNewChat();
+   }
+
+   }catch(err){
+      console.log(err);
+   }
+}
+
     return(
         <section className="sidebar">
          {/* new chat button */}
@@ -61,7 +79,14 @@ function Sidebar(){
                allThreads?.map((thread, idx) => (
                <li key={idx} 
                onClick={(e) => changeThread(thread.threadId)}>
-               {thread.title}</li>
+               {thread.title}
+               <i className="fa-solid fa-trash"
+                 onClick={(e) => {
+                  e.stopPropagation();//stop event bubbling
+                  deleteThread(thread.threadId);
+                 }}
+               ></i>
+               </li>
                ))
             }
          </ul>
